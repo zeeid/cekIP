2f8cf22e0b3a3afea5a9b6c5d5d1d7b4b9040196
ZeeiDev <zeeideveloper19@gmail.com>
2022-11-26 19:52:49 +0700

    cek

--- index.js
+++ index.js
@@ -10,7 +10,30 @@ app.use(expressip().getIpInfoMiddleware);
 app.set("PORT", PORT);
 
 app.get('/', function (req, res) {
-    res.send(req.ipInfo);
+  // {
+  //   "ip": "182.253.87.181",
+  //   "range": [
+  //   3070056192,
+  //   3070056447
+  //   ],
+  //   "country": "ID",
+  //   "region": "",
+  //   "eu": "0",
+  //   "timezone": "Asia/Jakarta",
+  //   "city": "",
+  //   "ll": [
+  //   -6.175,
+  //   106.8286
+  //   ],
+  //   "metro": 0,
+  //   "area": 1000
+  //   }
+
+  const ipInfo = req.ipInfo;
+  console.log(ipInfo);
+  var message = `Hey, you are browsing from ${ipInfo.city}, ${ipInfo.country}`;
+  res.send(message);
+    // res.send(req.ipInfo);
 });
 
 app.listen(app.get('PORT'), function () {
